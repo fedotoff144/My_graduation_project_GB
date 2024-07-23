f887366066fd9074f0a31dba75bba21973d8754e
fedotoff144 TecnoLaptop <fedotoff144@gmail.com>
2024-07-23 16:59:08 +0300

    started working with models

---
+++ recipesbook/userapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photo/')
